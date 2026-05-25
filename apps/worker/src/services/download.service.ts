782efbf59be63628180d54c4a26d0201b6e7f670
Ritabrata Ghosh <76sonali40@gmail.com>
2026-05-25 19:46:45 +0530

    fix: create temp dir dynamically

--- apps/worker/src/services/download.service.ts
+++ apps/worker/src/services/download.service.ts
@@ -3,11 +3,20 @@ import path from "path";
 import axios from "axios";
 
 export const downloadFile = async (url: string, filename: string) => {
+  const tempDir = path.resolve(__dirname, "../../temp");
+
+  // ensure temp folder exists
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir, {
+      recursive: true,
+    });
+  }
+
   const response = await axios.get(url, {
     responseType: "arraybuffer",
   });
 
-  const tempPath = path.resolve(__dirname, "../../temp", filename);
+  const tempPath = path.join(tempDir, filename);
 
   fs.writeFileSync(tempPath, response.data);
 
